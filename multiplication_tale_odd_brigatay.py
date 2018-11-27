aa278b1f7531a43e70e9a9114c0f7934681db7f3
nielandrew <45379393+nielandrew@users.noreply.github.com>
2018-11-27 11:48:18 +0800

    Create multiplication_tale_odd_brigatay.py

---
+++ multiplication_tale_odd_brigatay.py
@@ -0,0 +1,8 @@
+for x in range (1,11):
+    for y in range (1,11):
+        z = x * y
+        if z % 2 == 1:
+            print("O",end="\t")
+        else:
+            print(x*y,end="\t")
+    print()
